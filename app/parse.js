9faafeda7d7c7214eb8df8995afa60c31f01d09a
anhilo <anhilo@users.noreply.github.com>
2019-01-03 11:31:44 +0800

    Update parse.js

--- app/parse.js
+++ app/parse.js
@@ -1,17 +1,28 @@
 const request = require('request')
 const cheerio = require('cheerio')
+const Iconv = require('iconv-lite');
 // const iconvLite = require('iconv-lite')
 const fs = require('fs')
 const {downloadImg} = require('./utils')
 const {UA} = require('./const')
 let retryCount = 0;
-
+const electron = require('electron');
+// Module to control application life.
+const {
+  app,
+  Menu,
+  ipcMain,
+  BrowserWindow,
+  session
+} = electron;
+var j = request.jar();
 let option = {
   maxRedirects: 15,
-  jar:true, // 记住cookie
+  jar:j, // 记住cookie
   headers: {
     'User-Agent': UA,
-  }
+  },
+  encoding: null
 }
 
 
@@ -24,30 +35,86 @@ function getHtml(url,win) {
   option.headers.referer = url;
   return new Promise((resolve,reject) => {
     request(option,(error,resp,body)=>{
-      if (!error && resp.statusCode == 200) {
-        let $ = cheerio.load(body);
-        if ($('#loginchina').length) {
-          // TODO: 有登录，可能存在一直重定向到登录页面，需要改造
-          if(retryCount<20){
-            retryCount++;
-            win.send('logger',`第${retryCount}重试请求目标网页`);
-            getHtml(url,win);
-          }else{
-            win.send('logger','重试请求目标网页达到上限值：20。终止请求');
-            reject('重定向次数过多');
-          }
-        }else{
-          win.send('logger','需要登录');
-          resolve(body);
-        }
-      }else {
-        win.send('logger',error);
-        reject(error)
-      }
-    })
-  })
+      // if (!error && resp.statusCode == 200) {
+      //   let $ = cheerio.load(body);
+      //   if ($('#loginchina').length) {
+      //     // TODO: 有登录，可能存在一直重定向到登录页面，需要改造
+      //     if(retryCount<20){
+      //       retryCount++;
+      //       win.send('logger',`第${retryCount}重试请求目标网页`);
+      //       //getHtml(url,win);
+      //     }else{
+      //       win.send('logger','重试请求目标网页达到上限值：20。终止请求');
+      //       reject('重定向次数过多');
+      //     }
+      //   }else{
+      //     win.send('logger','需要登录');
+      //     resolve(body);
+      //   }
+      // }else {
+      //   win.send('logger','error');
+      //   //reject(error)
+      // }
+      //win.send('logger','aaa'+error);
+      let $ = cheerio.load(body);
+      win.send('logger',body);
+    //   if ($('#loginchina').length) {
+    //   //   win.send('logger',body);
+    //     // win.send('logger',option.jar);
+    //         var opt1 = {
+    //                 width: 1000,
+    //                 height: 660,
+    //                 minWidth: 1000,
+    //                 minHeight: 660,
+    //                 title: "1688图片下载器",
+    //                 // icon: path.join(__dirname, 'icons', 'icon.ico')
+    //               };
 
+    //               // if(process.platform=='linux'){
+    //               //   opt.icon = path.join(__dirname, 'icons', 'icon.png');
+    //               // }
+
+
+    //               // Create the browser window.   http://electron.atom.io/docs/api/browser-window/
+    //               win1 = new BrowserWindow(opt1);
+
+    //               // win.setTitle(custom.title || "OSS Browser");
+    //               // win.loadURL(`file://${__dirname}/index.html`);
+    //               win1.loadURL('https://login.1688.com/member/signin.htm?from=sm&Done='+option.url);
+    //               // win1.setMenuBarVisibility(false);
+    //               win1.show();
+    //               win1.on('closed', ()=> {
+    //                 // win.send('logger','login is closed');
+    //                 // session.defaultSession.cookies.get({ url: 'https://1688.com' }, (error, cookies) => {
+    //                 //   for(var i=0;i<cookies.length;i++){
+    //                 //     option.jar.setCookie(request.cookie("'"+cookies[i].name+"="+cookies[i].value+"'"),'https://1688.com');
+    //                 //   }
+    //                   session.defaultSession.cookies.get({ url: 'https://detail.1688.com'},(error,cookies) =>{
+    //                     cookies.forEach(cookie => {
+    //                       j.setCookie(request.cookie(`${cookie.name}=${cookie.value};`),'https://detail.1688.com');
+    //                     });
+    //                   })
+    //                 //   // option.cookies = cookies;
+    //                 // })
+    //                 // return  win1.webContents.getAllWebContents();
+    //               })
+
+
+  
+    //   // }else{
+    //   //   win.send('logger','bbbbb');
+    //   // }
+    //   // 
+
+    // }
+    // else{
+    //   resolve(body);
+    //   // win.send('loggger','inlogined');
+    // }
+  })
+})
 }
+
 
 /**
  * 获取详情图片原始链接
@@ -106,7 +173,7 @@ async function downAli(link, path, win) {
   retryCount = 0;
   win.send('logger','正在请求目标网页');
   getHtml(link,win).then((html)=>{
-    const $ = cheerio.load(html)
+    const $ = cheerio.load(Iconv.decode(html, 'gb2312').toString())
     const productTitle = $('#mod-detail-title .d-title').text()
     path += `/${productTitle}`
     fs.mkdir(path, function (err) {
